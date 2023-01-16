4743698f5964db770e8c6a219d9508c218616a86
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-01-16 19:03:50 +0000

    some done

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,51 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px",
+    position: "absolute",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  const handelPostion = (event) => {
+    const newpostion = {
+      left: "100px",
+      top: "400px",
+      position: "absolute",
+    }
+    setBallPosition(newpostion);
+  }
+  const reset = () => {
+    setRenderBall(false);
+  };
+
+  const giveStyle = () => {
+    console.log(ballPosition);
+    return (
+      ballPosition
+    )
+  }
+
+
+  const renderChoice = () => {
+    if (renderBall === false) {
+      return (<button onClick={() => { setRenderBall(true) }} className="start ballProvider">
+        Start
+      </button>)
+    } else {
+      return (
+        <div className="ball" style={giveStyle()} onKeyDown={handelPostion}></div>
+      )
+    }
+  };
+
 
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
         Reset
       </button>
+
       {renderChoice()}
     </div>
   );
