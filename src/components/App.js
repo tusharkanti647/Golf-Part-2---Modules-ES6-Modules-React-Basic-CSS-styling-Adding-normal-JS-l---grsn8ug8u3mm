2b820568b9f3b6d2008c1ea9517427bf9c6e3fcb
tushar <tusharkanti647@gmail.com>
2023-01-21 16:21:51 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,62 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+import { useEffect } from "react";
 
 const App = () => {
-  const [renderBall, setRenderBall] = useState(false);
+  const [isBallVisible, setIsBallVisible] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition, setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
-    position: "absolute",
-  });
 
-  const handelPostion = (event) => {
-    const newpostion = {
-      left: "100px",
-      top: "400px",
-      position: "absolute",
-    }
-    setBallPosition(newpostion);
-  }
   const reset = () => {
-    setRenderBall(false);
+    setIsBallVisible(false);
+    setX(0);
+    setY(0);
   };
-
-  const giveStyle = () => {
-    console.log(ballPosition);
-    return (
-      ballPosition
-    )
-  }
-
 
   const renderChoice = () => {
-    if (renderBall === false) {
-      return (<button onClick={() => { setRenderBall(true) }} className="start ballProvider">
-        Start
-      </button>)
-    } else {
+    if (isBallVisible) {
       return (
-        <div className="ball" style={giveStyle()} onKeyDown={handelPostion}></div>
-      )
+        <div className="ball" style={{ left: `${x}px`, top: `${y}px` }}></div>
+      );
     }
+
+    return (
+      <button onClick={() => setIsBallVisible(true)} className="start">
+        Start
+      </button>
+    );
   };
 
+  useEffect(() => {
+    const handler = (event) => {
+      if (event.key === "ArrowRight") {
+        setX(x + 5);
+      }
+      if (event.key === "ArrowUp") {
+        setY(y - 5);
+      }
+      if (event.key === "ArrowLeft") {
+        setX(x - 5);
+      }
+      if (event.key === "ArrowDown") {
+        setY(y + 5);
+      }
+    };
+
+    window.addEventListener("keydown", handler);
+
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  }, [x, y]);
 
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
         Reset
       </button>
-
       {renderChoice()}
     </div>
   );
 };
-
 export default App;
